3229d20118f90f37c28771384971d4cf14c73586
haris.khan636 <harisjriv@gmail.com>
2023-05-17 16:42:17 +0500

    final

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'text_pusher';
+
+  itemForm!: FormGroup;
+  data = '';
+  array: any[] = [];
+  count = 0;
+
+  ngOnInit() {
+    this.itemForm = new FormGroup({
+      item: new FormControl('', [Validators.required]),
+    });
+  }
+
+  pushText() {
+    this.count++;
+    this.array.push(this.data);
+    this.data = '';
+  }
+
+  popText(i: number) {
+    this.array.splice(i, 1);
+    this.count--;
+  }
 }
